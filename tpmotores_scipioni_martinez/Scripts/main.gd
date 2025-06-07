e0428836e360278b4df01c057e1c2549261ce7b4
Rodrigo_Scipioni <rodrigoscipioni1@gmail.com>
2025-06-07 15:35:41 -0300

    Agregué enemigo y cond. de victoria en main

--- tpmotores_scipioni_martinez/Scripts/main.gd
+++ tpmotores_scipioni_martinez/Scripts/main.gd
@@ -1,1 +1,12 @@
 extends Node2D
+
+
+func _on_area_2d_body_entered(body: Node2D):
+	if body.name == "Personaje":
+		$Enemigo.queue_free()
+		$Bandera.queue_free()
+		pass
+	
+	if body.name == "Enemigo":
+		$Personaje.queue_free()
+		pass
