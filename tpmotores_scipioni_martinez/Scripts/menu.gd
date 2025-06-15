4caa86c9f8587581ef90cb1eaeb14f058dad3d64
FacundoTupacMM <tuu.x@live.com>
2025-06-14 21:03:17 -0300

    Update menu a medio hacer

--- tpmotores_scipioni_martinez/Scripts/menu.gd
+++ tpmotores_scipioni_martinez/Scripts/menu.gd
@@ -1,7 +1,17 @@
 extends Node2D
 
 signal jugar
+signal salir
+signal rejugar
 
 func _on_jugar_pressed():
 	emit_signal("jugar")
+	pass
+
+func _on_salir_pressed():
+	emit_signal("salir")
+	pass
+
+func _on_rejugar_pressed():
+	emit_signal("rejugar")
 	pass
