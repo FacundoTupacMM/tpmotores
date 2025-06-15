77a2285d7c443fcbc89d4c6038b980eb65bfe54a
FacundoTupacMM <tuu.x@live.com>
2025-06-14 23:25:54 -0300

    Update SSMain

--- tpmotores_scipioni_martinez/Scripts/ss_main.gd
+++ tpmotores_scipioni_martinez/Scripts/ss_main.gd
@@ -1,5 +1,5 @@
 extends Control
 
 func _on_animation_player_animation_finished(anim_name: StringName):
-	get_tree().change_scene("res://Menu.tscn")
+	get_tree().change_scene_to_file("res://Escenas/main.tscn")
 	pass
