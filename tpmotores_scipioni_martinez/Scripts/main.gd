5b1b83a3882892144b79a6d9cd577c4433771396
FacundoTupacMM <tuu.x@live.com>
2025-06-14 21:54:46 -0300

    Agrego botones rejugar y salir

--- tpmotores_scipioni_martinez/Scripts/main.gd
+++ tpmotores_scipioni_martinez/Scripts/main.gd
@@ -9,11 +9,24 @@ func _on_menu_jugar():
 	$Menu.visible = false
 	pass
 
-func _on_bandera_gana() -> void:
+func _on_bandera_gana():
 	$Enemigo.queue_free()
 	$Bandera.queue_free()
+	get_tree().paused = true
+	$Menu.visible = true
 	pass
 
-func _on_bandera_pierde() -> void:
+func _on_bandera_pierde():
 	$Personaje.queue_free()
+	get_tree().paused = true
+	$Menu.visible = true
+	pass
+
+func _on_menu_rejugar():
+	get_tree().paused = false
+	$Menu.visible = false
+	pass
+
+func _on_menu_salir():
+	get_tree().quit()
 	pass
