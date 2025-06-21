54824c7ea8744175a24cc6768f399ce8fed230d1
FacundoTupacMM <tuu.x@live.com>
2025-06-20 22:36:36 -0300

    Update enemigo 2

--- tpmotores_scipioni_martinez/Scripts/enemigo2.gd
+++ tpmotores_scipioni_martinez/Scripts/enemigo2.gd
@@ -1,11 +1,12 @@
 extends CharacterBody2D
 
 var movimiento = Vector2()
-var velocidad = 1.5
+var velocidad = 150
 
 func _physics_process(_delta):
-	move_and_collide(movimiento)
-	set_vector(get_node("../Bandera").global_position - global_position)
+	velocity = movimiento
+	move_and_slide()
+	set_vector(get_node("../Personaje").global_position - global_position)
 	Enemigo_derecha()
 	Enemigo_izquierda()
 	pass
@@ -13,13 +14,13 @@ pass
 
 func Enemigo_derecha():
 	if movimiento.x > 0:
-		$AnimationPlayer.play("Enemy_derecha")
+		$AnimationPlayer.play("Enemy2_derecha")
 	pass
 pass
 
 func Enemigo_izquierda():
 	if movimiento.x < 0:
-		$AnimationPlayer.play("Enemy_izquierda")
+		$AnimationPlayer.play("Enemy2_izquierda")
 	pass
 pass
 	
