5e81339599654fdd109a358af91a2bbbc4a706a0
FacundoTupacMM <tuu.x@live.com>
2025-06-22 18:47:17 -0300

    Create creditos

---
+++ tpmotores_scipioni_martinez/Scripts/creditos.gd
@@ -0,0 +1,13 @@
+extends Button
+
+func _on_mouse_entered():
+	text = "Rodrigo Scipioni
+Facundo Tupac Martinez
+Natalia Florencia Villar Vasallo"
+	pass
+pass
+
+func _on_mouse_exited():
+	text = "Creditos"
+	pass
+pass
